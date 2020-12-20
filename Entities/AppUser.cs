c5d7564270aa33c9db36e258cbd83f5db252d619
Aleix Oliva <aolivga@gmail.com>
2020-12-20 13:50:23 +0100

    Modified DB and Async

---
+++ Entities/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        /* FULL DEFINITION PROPERTIES
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+        */
+    }
+}
