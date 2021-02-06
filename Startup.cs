e1d1763fec6a6c9bb7ba3217b1530025dd0cb8e6
Aleix Oliva <aolivga@gmail.com>
2021-02-06 13:06:56 +0100

    Error tests API

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 namespace API
 {
@@ -47,27 +48,26 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
             services.AddCors();
-            services.AddIdentityServices(_config);           
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+                //  if (env.IsDevelopment())
+                //app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                //}
+
+                app.UseMiddleware<ExceptionMiddleware>();
+                app.UseHttpsRedirection();
+                app.UseRouting();
+                app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+                app.UseAuthentication();
+                app.UseAuthorization();
+                app.UseEndpoints(endpoints => {endpoints.MapControllers();});
+            
         }
     }
 }
